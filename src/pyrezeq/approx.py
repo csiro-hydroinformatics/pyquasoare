fix: worked on fixing numerical pb

src/pyrezeq/approx.py
@@ -1,3 +1,4 @@
+from itertools import product as prod
 import math
 import numpy as np
 
@@ -5,6 +6,8 @@ from pyrezeq import has_c_module
 if has_c_module():
     import c_pyrezeq
     REZEQ_EPS = c_pyrezeq.get_eps()
+    REZEQ_CONTINUITY_ATOL = c_pyrezeq.get_continuity_atol()
+    REZEQ_CONTINUITY_RTOL = c_pyrezeq.get_continuity_rtol()
     REZEQ_NFLUXES_MAX = c_pyrezeq.get_nfluxes_max()
 else:
     raise ImportError("Cannot run rezeq without C code. Please compile C code.")
@@ -145,15 +148,51 @@ def approx_fun_from_matrix(alphas, nu, a_matrix, b_matrix, c_matrix, x):
     return outputs
 
 
+def isequal(f1, f2, atol=REZEQ_CONTINUITY_ATOL, \
+                    rtol=REZEQ_CONTINUITY_RTOL):
+    """ Checking if two values are equal """
+    errmax = atol+rtol*np.abs(f1)
+    return np.abs(f1-f2)<errmax
+
+def notequal(f1, f2, atol=REZEQ_CONTINUITY_ATOL, \
+                    rtol=REZEQ_CONTINUITY_RTOL):
+    return 1-isequal(f1, f2, atol, rtol)
+
+
+def check_continuity(alphas, nu, a_matrix, b_matrix, c_matrix):
+    nalphas = len(alphas)
+    nfluxes = a_matrix.shape[1]
+    fevals = np.zeros((nalphas-1, nfluxes, 2))
+    for i, j in prod(range(nfluxes), range(nalphas-1)):
+        a0, a1 = alphas[[j, j+1]]
+        a, b, c = a_matrix[j, i], b_matrix[j, i], c_matrix[j, i]
+        fevals[j, i, 0] = approx_fun(nu, a, b, c, a0)
+        fevals[j, i, 1] = approx_fun(nu, a, b, c, a1)
+
+    return np.all(isequal(fevals[1:, :, 0], fevals[:-1, :, 1]))
+
+
 def optimize_nu(funs, alphas, scalings_ref, nexplore=1000):
     """ Optimize nu and compute corresponding coefficients """
-
     a0, a1 = alphas.min(), alphas.max()
-    theta_max = max(6., max(abs(math.asinh(a0)), abs(math.asinh(a1))))
-    theta_min = -theta_max
+
+    # nu.S varies from [nu.a0, nu.a1]
+    # if r = [abs(a0), abs(a1)]
+    # hence abs(nu.S) varies in [nu.min(r), nu.max(r)]
+    # we want this to be in [1e-5, 1e2]
+    theta_min = math.log(1e-3)
+    aamin = min(abs(a0), abs(a1))
+    if aamin>1e-3:
+        theta_min += math.log(aamin)
+
+    aamax = max(abs(a0), abs(a1))
+    theta_max = math.log(5e1)-math.log(aamax)
+
     nalphas = len(alphas)
     assert len(funs) == len(scalings_ref)
     assert np.all(np.abs(scalings_ref)>10*REZEQ_EPS)
+
+    nfluxes = len(funs)
 
     # Error evaluation points for each band
     eps = np.array([1./4, 0.5, 3./4])[None, :]
@@ -178,58 +217,50 @@ def optimize_nu(funs, alphas, scalings_ref, nexplore=1000):
             yt = y_true[j]
             err = approx_fun(nu, a, b, c, x)-yt
             of += (err*err).mean()
+            # Check continuity
+            if j>0:
+                f = approx_fun(nu, a, b, c, alphaj)
+                # Eliminate configurations with non continuous
+                # approximation
+                if notequal(fprev, f):
+                    return 1e100
+
+            fprev = approx_fun(nu, a, b, c, alphajp1)
+
         return of
 
     # Systematic exploration
     fobjexp = lambda x: fobj(math.exp(x))
-    xa, xb = -5, 5
-    ntry = 1000
+
+    xa, xb = theta_min, theta_max
+    ntry = 50
     xx = np.linspace(xa, xb, ntry)
     ff = np.array([fobjexp(x) for x in xx])
     imin = np.argmin(ff)
     xa, xb = xx[max(0, imin-1)], xx[min(ntry-1, imin+1)]
 
+    # Golden ratio minimization
+    dx = 10
     niter= ntry
-    theta = xx[imin]
+    niter_max = 500
+    tol = 1e-2
+    invphi = (math.sqrt(5)-1)/2
+    while abs(dx)>tol and niter<niter_max:
+        dx = xb-xa
+        xc = xb-dx*invphi
+        xd = xa+dx*invphi
+        if fobjexp(xc)<fobjexp(xd):
+            xb = xd
+        else:
+            xa = xc
+
+        niter += 1
+
+    theta = (xa+xb)/2
     fopt = fobjexp(theta)
     nu = math.exp(theta)
 
-    ## Golden ratio minimization
-    #dx = 10
-    #niter = 0
-    #niter_max = 500
-    #tol = 1e-2
-    #invphi = (math.sqrt(5)-1)/2
-    #while abs(dx)>tol and niter<niter_max:
-    #    dx = xb-xa
-    #    xc = xb-dx*invphi
-    #    xd = xa+dx*invphi
-    #    if fobjexp(xc)<fobjexp(xd):
-    #        xb = xd
-    #    else:
-    #        xa = xc
-
-    #    niter += 1
-
-    #theta = (xa+xb)/2
-    #fopt = fobjexp(theta)
-    #nu = math.exp(theta)
-
     # Get coefficient for unscaled functions
     amat, bmat, cmat = get_coefficients_matrix(funs, alphas, nu)
-
-    # Check continuity
-    f = []
-    for j in range(nalphas-1):
-        a0, a1 = alphas[[j, j+1]]
-        a, b, c = amat[j].sum(), bmat[j].sum(), cmat[j].sum()
-        f.append([\
-            approx_fun(nu, a, b, c, a0), \
-            approx_fun(nu, a, b, c, a1)
-           ])
-    f = np.array(f)
-    assert np.allclose(f[1:, 0], f[:-1, 1])
-    import pdb; pdb.set_trace()
-
     return nu, amat, bmat, cmat, niter, fopt
 

src/pyrezeq/slow.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.integrate import solve_ivp
 
-from pyrezeq.approx import REZEQ_EPS
+from pyrezeq.approx import REZEQ_EPS, isequal
 
 def integrate_forward_numerical(funs, dfuns, t0, s0, t, \
                             method="Radau", max_step=np.inf, \
@@ -74,9 +74,6 @@ def routing_numerical(delta, theta, q0, s0, \
 # --- REZEQ functions translated from C for slow implementation ---
 def isnull(x):
     return 1 if abs(x)<REZEQ_EPS else 0
-
-def isequal(x, y):
-    return 1 if abs(x-y)<REZEQ_EPS else 0
 
 def notnull(x):
     return 1-isnull(x)
@@ -150,7 +147,7 @@ def integrate_forward(nu, a, b, c, t0, s0, t):
     if t-t0>dtmax:
         return np.nan
 
-    if isequal(t0, t):
+    if isequal(t0, t, REZEQ_EPS, 0.):
         return s0
 
     if isnull(b) and isnull(c):
@@ -386,49 +383,8 @@ def integrate(alphas, scalings, nu, \
 
         # Check continuity
         if nit>1:
-            continuity_error_max = REZEQ_EPS*1e2+abs(funval_prev)*1e-5
-            if abs(funval-funval_prev)> continuity_error_max:
-                raise ValueError("continuity problem, |f-fprev|>"\
-                                    +f"{continuity_error_max:3.3e}")
-
-        # Check integration up to the next band limit
-        #if notnull(funval):
-        #    if isneg(funval):
-        #        # non-increasing function -> move to lower band
-        #        jalpha_next = jalpha if extrapolating_high else max(jmin, jalpha-1)
-        #        # .. need to substract 2EPS to avoid being stuck as extrapolation
-        #        if extrapolating_high:
-        #            s_end = alpha_max-2*REZEQ_EPS
-        #        else:
-        #            s_end = alpha0
-        #            s_end = alpha0-2*REZEQ_EPS if isequal(s_end, s_start) else alpha0
-
-        #    elif ispos(funval):
-        #        # increasing function -> move to upper band
-        #        jalpha_next = jalpha if extrapolating_low else min(jmax, jalpha+1)
-        #        # .. need to add 2EPS to avoid being stuck as extrapolation
-        #        if extrapolating_low:
-        #            s_end = alpha_min+2*REZEQ_EPS
-        #        else:
-        #            s_end = alpha1
-        #            s_end = alpha1+2*REZEQ_EPS if isequal(s_end, s_start) else alpha1
-
-        #    # Compute time for which s(t) = s_end
-        #    t_end = t_start+integrate_inverse(nu, aoj, boj, coj, s_start, s_end)
-        #else:
-        #    # derivative is null -> finish iteration
-        #    jalpha_next = jalpha
-        #    t_end = t_final
-        #    s_end = s_start
-
-        ## Set time to end of time step if finished iteration (t_end>t_final)
-        ##    or if t1 is nan (i.e. close to steady or never reaching t_final)
-        #t_end = t_final if t_end>t0+delta or np.isnan(t_end) or t_end<t_start else t_end
-
-        ## Recompute s_end - required only if finished iteration or
-        ## extrapolating. Skip if funval is null => steady
-        #if notnull(funval):
-        #    s_end = integrate_forward(nu, aoj, boj, coj, t_start, s_start, t_end)
+            if approx.notequal(funval_prev, funval):
+                raise ValueError("continuity problem")
 
         # Try integrating up to the end of the time step
         s_end = integrate_forward(nu, aoj, boj, coj, t_start, s_start, t_final)

tests/test_approx.py
@@ -24,7 +24,7 @@ LOGGER = iutils.get_logger("approx", flog=FTEST / "test_approx.log")
 @pytest.fixture(scope="module", \
             params=["x2", "x4", "x6", "x8", "tanh", "exp", "sin", \
                             "recip", "recipquad", "runge", "stiff", \
-                            "ratio", "logistic"])
+                            "ratio", "logistic", "genlogistic"])
 def reservoir_function(request, selfun):
     name = request.param
     if name!=selfun and selfun!="":
@@ -130,6 +130,14 @@ def reservoir_function(request, selfun):
         inflow = 0.
         sol = lambda t, s0: s0*np.exp(lam*t)/(1-s0+s0*np.exp(lam*t))
         alpha0, alpha1 = 0., 3.
+
+    elif name == "genlogistic":
+        K, nu, alpha = 10., 0.1, 2
+        fun = lambda x: alpha*(1-(x/K)**nu)*x
+        dfun = lambda x: alpha*(1-(x/K)**nu)-a*nu(x/K)**nu
+        inflow = 0.
+        sol = lambda t, s0: K/(1+((K/s0)*nu-1)*math.exp(-alpha*nu*t))**(1./nu)
+        alpha0, alpha1 = 0, K
 
 
     #elif name == "cos":
@@ -267,14 +275,23 @@ def test_approx_fun(allclose, generate_samples):
 def test_get_coefficients(allclose, reservoir_function):
     fname, fun, dfun, _, _, (alpha0, alpha1) = reservoir_function
     nus = [0.01, 0.1, 1, 5]
+
+    #nus = [5]
+    # check fun = genlogisitc
+
     corrected = 0
     for nu in nus:
         a, b, c, corr = approx.get_coefficients(fun, alpha0, alpha1, nu)
         corrected += corr
 
         # Check function on bounds
-        assert allclose(approx.approx_fun(nu, a, b, c, alpha0), fun(alpha0), atol=1e-7)
-        assert allclose(approx.approx_fun(nu, a, b, c, alpha1), fun(alpha1), atol=1e-7)
+        fa = approx.approx_fun(nu, a, b, c, alpha0)
+        ft = fun(alpha0)
+        assert allclose(ft, fa, atol=1e-7)
+
+        fa = approx.approx_fun(nu, a, b, c, alpha1)
+        ft = fun(alpha1)
+        assert allclose(ft, fa, atol=1e-7)
 
         # Check mid point values
         if not corrected:
@@ -355,11 +372,15 @@ def test_get_coefficients_matrix(allclose, reservoir_function):
 def test_optimize_nu(allclose, reservoir_function):
     fname, fun, dfun, sol, inflow, (alpha0, alpha1) = reservoir_function
     funs = [lambda x: inflow+fun(x)]
-    nalphas = 11
+
+    # ratio function cannot be captured with 11 bands
+    # increase this to 31
+    nalphas = 31 if fname == "ratio" else 11
 
     alphas = np.linspace(alpha0, alpha1, nalphas)
     scr = np.ones(len(funs))
     nu, amat, bmat, cmat, niter, fopt = approx.optimize_nu(funs, alphas, scr)
+    assert approx.check_continuity(alphas, nu, amat, bmat, cmat)
 
     s = np.linspace(alpha0, alpha1, 10000)
     out = approx.approx_fun_from_matrix(alphas, nu, amat, bmat, cmat, s)
@@ -373,15 +394,17 @@ def test_optimize_nu(allclose, reservoir_function):
         "x6": 1e-3, \
         "x8": 1e-3, \
         "tanh": 5e-2, \
-        "exp": 5e-7,
+        "exp": 1e-7,
         "sin": 5e-3, \
         "recip": 5e-3, \
         "recipquad": 5e-2, \
         "runge": 1e-3, \
         "stiff": 1e-7, \
-        "ratio": 1.1, \
-        "logistic": 1e-8
+        "ratio": 0.8, \
+        "logistic": 1e-8, \
+        "genlogistic": 5e-3
     }
+
     assert rmse<rmse_thresh[fname]
     LOGGER.info("")
     LOGGER.info(f"[{fname}] optimize approx vs truth: "\
@@ -399,6 +422,7 @@ def test_optimize_vs_quad(allclose, reservoir_function):
     alphas = np.linspace(alpha0, alpha1, nalphas)
     scr = np.ones(len(funs))
     nu, amat, bmat, cmat, _, _ = approx.optimize_nu(funs, alphas, scr)
+    #assert approx.check_continuity(alphas, nu, amat, bmat, cmat)
 
     # Issue with continuity for fname=recip and
     # nu in [3.01, 3.03] -> TOFIX!
@@ -424,6 +448,15 @@ def test_optimize_vs_quad(allclose, reservoir_function):
 
     rmse_quad = math.sqrt(((fquad-ftrue)**2).mean())
 
+    #import matplotlib.pyplot as plt
+    #plt.plot(s, ftrue, label="true")
+    #plt.plot(s, fapprox, label="approx")
+    #plt.plot(s, fquad, label="quad")
+    #plt.legend()
+    #plt.show()
+    #import pdb; pdb.set_trace()
+
+
     # We want to make sure quad is always worse than app
     ratio = rmse/rmse_quad
 
@@ -437,7 +470,8 @@ def test_optimize_vs_quad(allclose, reservoir_function):
         "recip": 0.7, \
         "recipquad": 0.9, \
         "runge": 1.0+1e-4, \
-        "ratio": 0.2
+        "ratio": 0.9, \
+        "genlogistic": 0.9
     }
     assert ratio<ratio_thresh[fname]
     LOGGER.info("")

tests/test_integrate.py
@@ -393,7 +393,8 @@ def test_reservoir_equation_extrapolation(allclose, ntry, reservoir_function):
     # Optimize approx
     nalphas = 5
     alphas = np.linspace(alpha0, alpha1, nalphas)
-    nu, amat, bmat, cmat, niter, fopt = approx.optimize_nu(funs, alphas)
+    scr = np.ones(2)
+    nu, amat, bmat, cmat, niter, fopt = approx.optimize_nu(funs, alphas, scr)
 
     # Configure integration
     t0 = 0 # Analytical solution always integrated from t0=0!

tests/test_steady.py
@@ -118,7 +118,8 @@ def test_steady_state_scalings_gr4j(allclose):
         lambda x: -x*(2-x), \
         lambda x: -(4/9*x)**5/4
     ]
-    nu, amat, bmat, cmat, niter, fopt = approx.optimize_nu(funs, alphas)
+    scr = np.ones(3)
+    nu, amat, bmat, cmat, niter, fopt = approx.optimize_nu(funs, alphas, scr)
 
     nval = 200
     scalings = np.random.uniform(0, 100, size=(nval, 3))