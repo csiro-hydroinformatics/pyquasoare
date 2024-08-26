fix: removed flux sum from benchmarks and numerical integ

src/pyrezeq/slow.py
@@ -8,38 +8,53 @@ from pyrezeq.approx import REZEQ_EPS, REZEQ_PI
 from pyrezeq.approx import isequal, notequal, isnull, notnull
 from pyrezeq.integrate import quad_constants
 
-def integrate_numerical(sumfun, dsumfun, fluxes, dfluxes, t0, s0, t, \
+def integrate_numerical(fluxes, dfluxes, t0, s0, t, \
                             method="Radau", max_step=np.inf, \
-                            fun_args=None):
+                            scaling=None, v=None, m=None):
     method = method.title()
     nfluxes = len(fluxes)
     assert len(dfluxes) == nfluxes
+    multi_fluxes = nfluxes>1
 
-    v = np.zeros(nfluxes+1)
+    v = np.zeros(nfluxes+multi_fluxes) if v is None else v
     def fun_ivp(t, y):
-        v[0] = sumfun(y[0])
+        total = 0.
         for i in range(nfluxes):
-            v[i+1] = fluxes[i](y[0])
+            s = 1. if scaling is None else scaling[i]
+            f = fluxes[i](y[0])*s
+            total += f
+            v[i] = f
+
+        if multi_fluxes:
+            v[nfluxes] = total
         return v
 
-    m = np.zeros((nfluxes+1, nfluxes+1))
+    m = np.zeros((nfluxes+multi_fluxes, nfluxes+multi_fluxes)) \
+            if m is None else m
     jac_ivp = None
     if method == "Radau":
         def jac_ivp(t, y):
-            m[0, 0] = dsumfun(y[0])
+            total = 0.
             for i in range(nfluxes):
-                m[i+1, 0] = dfluxes[i](y[0])
+                s = 1. if scaling is None else scaling[i]
+                df = dfluxes[i](y[0])*s
+                total += df
+                m[i, 0] = df
+
+            if multi_fluxes:
+                m[nfluxes, 0] = total
             return m
+
+    y0 = [0.]*nfluxes+[s0] if multi_fluxes else [s0]
 
     res = solve_ivp(\
             fun=fun_ivp, \
             t_span=[t0, t[-1]], \
-            y0=[s0]+[0.]*nfluxes, \
+            y0=y0, \
             method=method, \
             max_step=max_step, \
             jac=jac_ivp, \
-            t_eval=t, \
-            args=fun_args)
+            t_eval=t)
 
     # Function evaluation
     nev = res.nfev
@@ -49,6 +64,32 @@ def integrate_numerical(sumfun, dsumfun, fluxes, dfluxes, t0, s0, t, \
         return np.array([]), np.array([]), nev, njac
     else:
         return res.t, res.y.T.squeeze(), nev, njac
+
+
+def numerical_model(fluxes, dfluxes, scalings, s0, timestep):
+    nval = scalings.shape[0]
+    sims = np.zeros(scalings.shape, dtype=np.float64)
+    niter = np.zeros(nval, dtype=np.int32)
+    s1 = np.zeros(nval, dtype=np.float64)
+    t0 = 0.
+    nfluxes = len(fluxes)
+    multi_fluxes = nfluxes>1
+    v = np.zeros(nfluxes+multi_fluxes)
+    m = np.zeros((nfluxes+multi_fluxes, nfluxes+multi_fluxes))
+
+    for t in range(nval):
+        # integrate
+        tn, send, nev, njac = slow.integrate_numerical(\
+                                        sfun, dsfun, \
+                                        funs, dfuns, \
+                                        0, s0, timestep, \
+                                        scaling=scalings[t], \
+                                        v=v, m=m)
+        s0 = send[0]
+        niter[t] = nev+njac
+        fluxes[t] = send[1:]
+
+    return niter, s1, fluxes
 
 
 # --- REZEQ functions translated from C for slow implementation ---
@@ -358,7 +399,7 @@ def quad_model(alphas, scalings, \
     t0 = 0.
 
     for t in range(nval):
-        niter[t], s1[t], fluxes[t] = quad_integrate(alphas, scalings, \
+        niter[t], s1[t], fluxes[t] = quad_integrate(alphas, scalings[t], \
                         a_matrix_noscaling, \
                         b_matrix_noscaling, \
                         c_matrix_noscaling, \
@@ -366,4 +407,5 @@ def quad_model(alphas, scalings, \
         s0 = s1[t]
 
     return niter, s1, fluxes
+
 

tests/test_benchmarks.py
@@ -146,17 +146,14 @@ def test_gr4jprod_vs_numerical(allclose):
                 P, E = inputs[t]
 
                 # fluxes equations
-                sumf, dsumf, fluxes, dfluxes = \
-                            benchmarks.gr4jprod_fluxes_scaled(P, E, X1)
+                fluxes, dfluxes = benchmarks.gr4jprod_fluxes_scaled(P, E, X1)
 
                 # Numerical integration
-                _, out, _, _ = slow.integrate_numerical(sumf, dsumf, \
-                                                fluxes, dfluxes, \
-                                                0, u_start, [1.])
-                numerical[t, 0] = out[0]*X1
-                numerical[t, 1] = out[1]*X1
-                numerical[t, 2] = -out[2]*X1
-                numerical[t, 3] = -out[3]*X1
+                _, out, _, _ = slow.integrate_numerical(fluxes, dfluxes, 0, u_start, [1.])
+                numerical[t, 0] = out[3]*X1
+                numerical[t, 1] = out[0]*X1
+                numerical[t, 2] = -out[1]*X1
+                numerical[t, 3] = -out[2]*X1
 
                 # loop
                 u_start = out[0]
@@ -264,10 +261,14 @@ def test_nonlinrouting_vs_numerical(allclose):
 
     for isite, siteid in enumerate(data_reader.SITEIDS):
         df = data_reader.get_data(siteid, "hourly")
-        # just 2022-Feb/March because it takes too much time otherwise
-        inflows = df.loc["2022-02":"2022-03", :].iloc[:, 0].values
-        q0 = inflows.mean()
-        theta = q0*timestep*10.
+        df = df.loc["2022-02-20": "2022-03-10"]
+
+        inflows = df.loc[:, "STREAMFLOW_UP[m3/sec]"].interpolate()
+        q0 = inflows.quantile(0.9)
+        inflows = inflows.values
+
+        # Stored volumne in 24h
+        theta = q0*24*timestep
 
         errmax_max = 0.
         for nu in nus:
@@ -285,9 +286,8 @@ def test_nonlinrouting_vs_numerical(allclose):
                             benchmarks.nonlinrouting_fluxes_scaled(qin, q0, theta, nu)
 
                 # Numerical integration
-                _, out, _, _ = slow.integrate_numerical(sumf, dsumf, \
-                                                fluxes, dfluxes, \
-                                                0, u_start, [timestep])
+                _, out, _, _ = slow.integrate_numerical(fluxes, dfluxes, 0, u_start, [timestep])
+                assert ~np.isnan(out[0])
                 numerical[t] = -out[2]*theta/timestep
 
                 # loop

tests/test_integrate.py
@@ -106,7 +106,8 @@ def test_delta_t_max(allclose, generate_samples):
         # Run solver first to see how far it goes
         f = lambda x: approx.quad_fun(a, b, c, x)
         df = lambda x: approx.quad_grad(a, b, c, x)
-        te, ns1, _, _= slow.integrate_numerical(f, df, [], [], t0, s0, t_eval)
+        te, ns1, _, _= slow.integrate_numerical([f], [df], t0, s0, t_eval)
+
         if len(te)==0:
             continue
 
@@ -123,8 +124,7 @@ def test_delta_t_max(allclose, generate_samples):
             t0, t1 = te[-3], te[-1]
             te = np.linspace(t0, 2*t1-t0, 500)
             s0 = ns1[-3]
-            te, ns1, nev, njac = slow.integrate_numerical(\
-                                                    f, df, [], [], t0, s0, te)
+            te, ns1, nev, njac = slow.integrate_numerical([f], [df], t0, s0, te)
             expected = te.max()
 
             # Compares with C code
@@ -215,8 +215,7 @@ def test_forward_vs_numerical(allclose, generate_samples):
 
         f = lambda x: approx.quad_fun(a, b, c, x)
         df = lambda x: approx.quad_grad(a, b, c, x)
-        te, expected, nev, njac = slow.integrate_numerical(f, df, [], [], \
-                                                            t0, s0, t_eval)
+        te, expected, nev, njac = slow.integrate_numerical([f], [df], t0, s0, t_eval)
         if len(te)<3:
             continue
 
@@ -527,10 +526,7 @@ def test_reservoir_equation(allclose, ntry, reservoir_function):
 
         # Numerical method
         start_exec = time.time()
-        tn, fn, nev, njac = slow.integrate_numerical(\
-                                        sfun, dsfun, \
-                                        funs, dfuns, \
-                                        t0, s0, t1)
+        tn, fn, nev, njac = slow.integrate_numerical(funs, dfuns, t0, s0, t1)
         end_exec = time.time()
         time_num += (end_exec-start_exec)*1e3
         numerical = fn[:, 0]
@@ -549,7 +545,7 @@ def test_reservoir_equation(allclose, ntry, reservoir_function):
                                                 s_start, timestep)
 
             # Check mass balance
-            #assert allclose(fluxes.sum()-s_end+s_start, 0.)
+            assert allclose(fluxes.sum()-s_end+s_start, 0.)
 
             # Python code
             n_slow, s_end_slow, fluxes_slow = slow.quad_integrate(alphas, scalings, \

tests/test_models.py
@@ -11,7 +11,7 @@ import scipy.integrate as sci_integrate
 
 from hydrodiy.io import iutils
 
-from pyrezeq import approx, models, slow, benchmarks, steady
+from pyrezeq import approx, models, slow, benchmarks
 
 from test_approx import generate_samples, reservoir_function
 
@@ -64,6 +64,14 @@ def test_quad_model(allclose):
             sims = np.column_stack([s1*X1, fx[:, 0]*X1, \
                                         -fx[:, 1]*X1, -fx[:, 2]*X1])
 
+            # Compare with slow
+            niter_slow, s1_slow, fx_slow = slow.quad_model(alphas, scalings, \
+                                            amat, bmat, cmat, s0, 1.)
+
+            sims_slow = np.column_stack([s1_slow*X1, fx_slow[:, 0]*X1, \
+                                        -fx_slow[:, 1]*X1, -fx_slow[:, 2]*X1])
+            assert allclose(sims, sims_slow)
+
             # Error analysis
             errmax = np.abs(sims-expected).max()
             errmax_max = max(errmax, errmax_max)
@@ -75,7 +83,7 @@ def test_quad_model(allclose):
 
             atol = 1e-3
             rtol = 1e-4
-            assert np.allclose(expected, sims, atol=atol, rtol=rtol)
+            assert allclose(expected, sims, atol=atol, rtol=rtol)
 
         mess = f"quad_model vs gr4jprod / site {isite+1}:"\
                     +f" errmax={errmax_max:3.3e}"\