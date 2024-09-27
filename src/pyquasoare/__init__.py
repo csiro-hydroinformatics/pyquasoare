e630e084336059a28977205d4bce3dbd76556d95
Julien Lerat <julien.lerat@csiro.au>
2024-09-27 15:34:21 +1000

    fix: fixed init

--- src/pyquasoare/__init__.py
+++ src/pyquasoare/__init__.py
@@ -1,14 +1,14 @@
 import importlib
 
 def has_c_module(raise_error=True):
-    name = f"c_pyrezeq"
+    name = f"c_pyquasoare"
     out = importlib.util.find_spec(name)
 
     if not out is None:
         return True
     else:
         if raise_error:
-            raise ImportError(f"C module c_pyrezeq is "+\
+            raise ImportError(f"C module c_pyquasoare is "+\
                 "not available, please run python setup.py build")
         else:
             return False
