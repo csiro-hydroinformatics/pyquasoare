94c678f8c56e188bfc4e95a41d22a532f7384874
Julien Lerat <julien.lerat@csiro.au>
2024-11-21 20:23:00 +1100

    fix: fixed example compilation

--- examples/quadroute/compile_unix.sh
+++ examples/quadroute/compile_unix.sh
@@ -1,9 +1,7 @@
 rm -f *.o, *.out, *.log, *.csv
-gcc -pedantic -Wall -Wextra -g -O3 -c ../../src/pyrezeq/c_rezeq_utils.c
-gcc -pedantic -Wall -Wextra -g -O3 -c ../../src/pyrezeq/c_rezeq_quad.c 
-gcc -pedantic -Wall -Wextra -g -O3 -c example.c
-gcc -pedantic -g -O3 *.o -lm -o example.out 
-#./example.out
+gcc -pedantic -Wall -Wextra -g -O3 -c ../../src/pyquasoare/c_quasoare_utils.c
+gcc -pedantic -Wall -Wextra -g -O3 -c ../../src/pyquasoare/c_quasoare_core.c 
+gcc -pedantic -Wall -Wextra -g -O3 -c example_quadroute.c
+gcc -pedantic -g -O3 *.o -lm -o example_quadroute.out 
+./example_quadroute.out
 
-# To check memory leaks
-valgrind -s --leak-check=yes ./example.out
