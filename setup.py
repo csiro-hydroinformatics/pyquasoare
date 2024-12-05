5b3eaef141bc1ec6d81ff01c2509cb163831e00b
Julien Lerat <julien.lerat@csiro.au>
2024-12-05 14:08:53 +1100

    chore: move build to pyproject

--- setup.py
+++ setup.py
@@ -1,71 +1,26 @@
 #!/usr/bin/env python
 
-import os
 import numpy
-from pathlib import Path
 
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 
-from Cython.Distutils import build_ext
-
-import versioneer
-
-def read(fname):
-    thisfolder = Path(__file__).resolve().parent
-    with (thisfolder / fname).open() as fo:
-        return fo.read()
-
-# C extensions
-ext_quasoare=Extension(name="c_pyquasoare",
-    sources=[
+extensions = [
+    Extension(name="c_pyquasoare",
+        sources=[
         "src/pyquasoare/c_pyquasoare.pyx",
         "src/pyquasoare/c_quasoare_utils.c",
         "src/pyquasoare/c_quasoare_core.c",
         "src/pyquasoare/c_nonlinrouting.c",
         "src/pyquasoare/c_gr4jprod.c"
-    ],
-    extra_cflags=["-O3"],
-    include_dirs=[numpy.get_include()])
+        ],
+        include_dirs=[numpy.get_include()])
+]
 
-cmdclass = versioneer.get_cmdclass()
-cmdclass["build_ext"] = build_ext
-
-# Package config
 setup(
     name="pyquasoare",
-    author= "Julien Lerat",
-    author_email= "julien.lerat@csiro.au",
-    url= "https://github.com/csiro-hydroinformatics/pyquasoare",
-    download_url= "hhttps://github.com/csiro-hydroinformatics/pyquasoare/tags",
-    version=versioneer.get_version(),
-    description= "Solve the approximate reservoir equation using the QuaSoARe method",
-    long_description= read("README.md"),
-    packages=find_packages(),
-    package_dir={"": "src"},
-    package_data={
-        "pyquasoare": [
-            "data/*.csv",
-            "data/daily/*.zip",
-            "data/hourly/*.zip"
-        ],
-    },
-    install_requires= [
-        "cython",
-        "hydrodiy",
-        "numpy >= 1.8.0",
-        "scipy (>=0.14.0)",
-        "pandas >= 0.16"
-    ],
-    cmdclass=cmdclass,
-    ext_modules=[ext_quasoare],
-    classifiers=[
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License"
-    ]
+    ext_modules=cythonize(extensions, \
+            compiler_directives={"language_level": 3, "profile": False})
 )
 
 
--- src/pyquasoare/__init__.py
+++ src/pyquasoare/__init__.py
@@ -13,9 +13,4 @@ def has_c_module(raise_error=True):
         else:
             return False
 
-#from ._version import get_versions
-#__version__ = get_versions()['version']
-#del get_versions
 
-from . import _version
-__version__ = _version.get_versions()['version']
