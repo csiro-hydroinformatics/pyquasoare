aef1a30fda45b0256cbbed736a809710a8c6607e
Julien Lerat <julien.lerat@csiro.au>
2025-01-28 16:40:16 +1100

    fix: fixed supplementary script

--- scripts/quasoare_paper_2024/supplementary_flux_computation.py
+++ scripts/quasoare_paper_2024/supplementary_flux_computation.py
@@ -40,7 +40,8 @@ siteid = "203024"
 
 flux_names = ["eff_rain", "actual_et", "percol"]
 flux_long_names = {
-    "eff_rain": "Effective rainfall",
+    "s": "Store filling level",
+    "eff_rain": "Infiltrated rain",
     "actual_et": "Actual ET",
     "percol": "Percolation"
     }
@@ -49,14 +50,14 @@ start_daily = "2010-01-01"
 end_daily = "2022-12-31"
 
 approx_long_names = {
-    "precise": "Precise computation",
-    "approx_midpt": "Approx. based on mid-point store",
-    "approx_midptfun": "Approx. based on mid-point flux"
+    "radau": "Radau",
+    "approx_midpt": "Approx - mid-point store",
+    "approx_midptfun": "Approx - mid-point flux"
     }
 
 # Plot config
 colors = {
-    "precise": "tab:blue",
+    "radau": "tab:blue",
     "approx_midpt": "tab:green",
     "approx_midptfun": "tab:orange"
     }
@@ -94,35 +95,51 @@ climate = np.column_stack([rain, evap])
 
 # Run models
 # .. model setup
-param = 10
+param = 50
 timestep = 1. # time step in days
 s0 = 0.5
 
 # .. fluxes functions
 fluxes, dfluxes = benchmarks.gr4jprod_fluxes_noscaling()
 
-LOGGER.info("Run model")
 X1 = param
 time_index = daily.index
 ones = np.ones(len(climate))
 scalings = [np.maximum(rain-evap, 0)/X1, \
                         np.maximum(evap-rain, 0)/X1, ones]
-scalings = scalings+[ones] if model_name.startswith("GRM") else scalings
 scalings = np.column_stack(scalings)
 
 # Numerical solver
+LOGGER.info("Run model - radau")
 niter, s1, sim = slow.numerical_model(fluxes, dfluxes,
                                       scalings, s0,
                                       timestep, method="Radau")
-sim = param*np.abs(sim) / timestep
+sim = param*np.abs(sim) * timestep
 
-cns = [f"{fn}_precise" for fn in flux_names]
+cns = [f"{fn}_radau" for fn in flux_names]
 sim = pd.DataFrame(sim, columns=cns, index=rain.index)
-sim.loc[:, "s"] = s1
+sim.loc[:, "s_radau"] = s1
+
+# Numerical solver, zoom on a particular time step
+LOGGER.info("Run model - radau - zoom")
+zdate = "2022-02-24"
+idx = np.where(time_index == zdate)[0][0]
+znsubsteps = 20
+zscalings = np.repeat(scalings[[idx]], znsubsteps, axis=0)
+zscalings[:, -1] = 1.
+zs0 = s1[idx-1]
+zniter, zs1, zsim = slow.numerical_model(fluxes, dfluxes,
+                                      zscalings, zs0,
+                                      timestep/znsubsteps, method="Radau")
+zsim = param*np.abs(zsim) * timestep / znsubsteps
+ztimes = pd.date_range("2022-02-24", "2022-02-25", periods=znsubsteps+1)[1:]
+zsim = pd.DataFrame(zsim, columns=cns, index=ztimes)
+zsim.loc[:, "s_radau"] = zs1
 
 # Get store at the beginning and end of timestep
-s_end = sim.s
-s_start = sim.s.shift(1)
+LOGGER.info("Compute fluxes approx")
+s_end = sim.s_radau
+s_start = sim.s_radau.shift(1)
 
 # Compute approximations of fluxes
 for ifx, fn in enumerate(flux_names):
@@ -148,46 +165,76 @@ for ifx, fn in enumerate(flux_names):
 LOGGER.info("Plot")
 plt.close("all")
 
-fig = plt.figure(figsize=(15, 12), layout="constrained")
-mosaic = [[f"{fn}/ts", f"{fn}/scat"] for fn in flux_names]
-kw = dict(width_ratios=[3, 1])
+fig = plt.figure(figsize=(16, 15), layout="constrained")
+mosaic = [[f"{fn}/ts", f"{fn}/scat" if fn != "s" else "."]
+          for fn in ["s"] + flux_names]
+kw = dict(width_ratios=[3, 1], hspace=0.08, wspace=0.08)
 axs = fig.subplot_mosaic(mosaic, gridspec_kw=kw)
 
 for iax, (aname, ax) in enumerate(axs.items()):
     fxn, ptype = aname.split("/")
-    df = sim.filter(regex=fxn, axis=1)
+    df = sim.filter(regex=f"^{fxn}_", axis=1)
     df.columns = [re.sub(f"{fxn}_", "", cn)
                   for cn in df.columns]
 
     if ptype == "ts":
         xlabel = ""
-        ylabel = "Flux [mm $day^{-1}$]"
-        title = "Simulated times series"
+        ylabel = "Store filling level [-]" if fxn == "s" \
+            else "Flux [mm $day^{-1}$]"
+        title = "simulated times series"
 
         for cn in df.columns:
-            lw = 2 if cn == "precise" else 1
-            se = df.loc["2022-01":"2022-03", cn]
+            lw = 3 if cn == "radau" else 2
+            se = df.loc["2022-02":"2022-03", cn]
             lab = approx_long_names[cn]
             ax.plot(se, lw=lw, color=colors[cn], label=lab)
     else:
-        xlabel = "Precise flux computation [mm $day^{-1}$]"
+        xlabel = "Radau flux computation [mm $day^{-1}$]" \
+            if iax == len(axs) - 1 else ""
         ylabel = "Approximated flux [mm $day^{-1}$]"
-        title = "Comparison approx. vs precise"
+        title = "approx. vs Radau"
 
-        x = df.filter(regex="precise", axis=1).squeeze()
+        x = df.filter(regex="radau", axis=1).squeeze()
         for cn in df.columns:
-            if cn == "precise":
+            if cn == "radau":
                 continue
 
             lab = approx_long_names[cn]
             ax.plot(x, df.loc[:, cn], "o", color=colors[cn], label=lab)
 
-    title = f"({letters[iax]}) {flux_long_names[fxn]}\n{title}"
+    title = f"({letters[iax]}) {flux_long_names[fxn]} - {title}"
     ax.set(title=title, xlabel=xlabel, ylabel=ylabel)
     if ptype == "ts":
-        ax.legend(loc=1)
+        loc = 1
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%d %b\n%y"))
+    else:
+        loc = 2
+    ax.legend(loc=loc, framealpha=0.)
 
 fp = fout / f"approximate_flux_approximation.png"
+fig.savefig(fp, dpi=fdpi)
+
+
+# Plot zoom simulation
+fig = plt.figure(figsize=(10, 8), layout="constrained")
+mosaic = [[f"{fn}/ts"] for fn in ["s_radau", "eff_rain_radau"]]
+axs = fig.subplot_mosaic(mosaic)
+for iax, (aname, ax) in enumerate(axs.items()):
+    varname, ptype = re.split("/", aname)
+    ax.plot(zsim.loc[:, varname], "-o",
+            color=colors["radau"], lw=3,
+            label="Radau")
+
+    fxn = re.sub("_radau", "", varname)
+    title = f"({letters[iax]}) {flux_long_names[fxn]}"
+    ylabel = "Store filling level [-]" if fxn == "s"\
+        else "Flux [mm]"
+    ax.set(title=title, ylabel=ylabel)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%d %b %y\n%H:%M"))
+
+    ax.legend(loc=7, framealpha=0.)
+
+fp = fout / f"approximate_flux_approximation_zoom.png"
 fig.savefig(fp, dpi=fdpi)
 
 LOGGER.completed()
